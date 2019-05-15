2bac39992f1333d73b27d504d29a352123ab5c23
KaiKai <1035759181@qq.com>
2019-05-16 01:45:15 +0800

    1. 求职者静态组件编写完成 2. 头像选择组件编写完成

--- src/components/avatar-selector/avatar-selector.jsx
+++ src/components/avatar-selector/avatar-selector.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { 
+  List,
+  Grid
+} from 'antd-mobile'
 
 class AvatarSelector extends React.Component {
+  constructor(props) {
+    super(props)
+    this.avatarList = []
+    for(let i = 0; i < 4; i++) {
+      this.avatarList.push({
+        text: `头像${i+1}`,
+        icon: require(`./images/icon-test_${i+1}.png`)
+      })
+    }
+  }
   render() {
+    const listHeader = '请选择头像'
     return (
-      <div>
-        AvatarSelector
-      </div>
+      <List renderHeader={()=> listHeader}>
+        <Grid
+          data={this.avatarList}
+          columnNum='4'
+        ></Grid>
+      </List>
     )
   }
 }
--- src/containers/personnel/personnel_info.jsx
+++ src/containers/personnel/personnel_info.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { 
+  NavBar,
+  InputItem,
+  TextareaItem,
+  Button
+} from 'antd-mobile'
+import AvatarSelector from '../../components/avatar-selector/avatar-selector'
 
 class PersonnelInfo extends React.Component {
   render() {
     return (
       <div>
-        PersonnelInfo
+        <NavBar>个人信息完善</NavBar>
+        <AvatarSelector />
+        <InputItem placeholder='请输入求职职位'>求职岗位：</InputItem>
+        <TextareaItem
+          title="个人介绍："
+          rows={3}
+           />
+        <Button type='primary'>保存</Button>
       </div>
     )
   }
