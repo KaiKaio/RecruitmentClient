f0b1149257e06644ca1d616908047009729453bf
KaiKai <1035759181@qq.com>
2019-06-09 21:53:49 +0800

    Updates

---
+++ precache-manifest.d7b2e8223d780c647453473b3b82aeb5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f908e6cfcd6ae51cadb7d48713575d0",
+    "url": "/RecruitmentClient/index.html"
+  },
+  {
+    "revision": "2d5f6ab523844cef40d9",
+    "url": "/RecruitmentClient/static/css/2.9a7581f7.chunk.css"
+  },
+  {
+    "revision": "2e4f88975dd7b920c5a8",
+    "url": "/RecruitmentClient/static/css/main.113e530f.chunk.css"
+  },
+  {
+    "revision": "2d5f6ab523844cef40d9",
+    "url": "/RecruitmentClient/static/js/2.8bac7649.chunk.js"
+  },
+  {
+    "revision": "2e4f88975dd7b920c5a8",
+    "url": "/RecruitmentClient/static/js/main.2165c906.chunk.js"
+  },
+  {
+    "revision": "43275394ed452c72ab81",
+    "url": "/RecruitmentClient/static/js/runtime~main.03d2cb3a.js"
+  },
+  {
+    "revision": "9304c99be30ccaf03b7e840eb1192361",
+    "url": "/RecruitmentClient/static/media/caidan-focus.9304c99b.png"
+  },
+  {
+    "revision": "57f5b01d5c04a2e3746e8a2fe9a3593b",
+    "url": "/RecruitmentClient/static/media/caidan.57f5b01d.png"
+  },
+  {
+    "revision": "eb73b46158ffb20a4aacdf6154fdf968",
+    "url": "/RecruitmentClient/static/media/renwu-focus.eb73b461.png"
+  },
+  {
+    "revision": "2b3344bfb0a8452a57ae2316c03a0fd0",
+    "url": "/RecruitmentClient/static/media/renwu.2b3344bf.png"
+  },
+  {
+    "revision": "df828f1cf44d7363b18939000988f4e7",
+    "url": "/RecruitmentClient/static/media/头像1.df828f1c.png"
+  },
+  {
+    "revision": "0376acb7d4c8a3a58290a3a326076ab7",
+    "url": "/RecruitmentClient/static/media/头像2.0376acb7.png"
+  },
+  {
+    "revision": "9545cfbe4fdf5f97be5cd3c10652703c",
+    "url": "/RecruitmentClient/static/media/头像3.9545cfbe.png"
+  },
+  {
+    "revision": "a1e007ac53a03ec876ab268ab96f1577",
+    "url": "/RecruitmentClient/static/media/头像4.a1e007ac.png"
+  }
+]);
