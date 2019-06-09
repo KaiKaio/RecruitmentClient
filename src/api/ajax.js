5da226724b52d98519151ff640eaa25966ecb67e
KaiKai <1035759181@qq.com>
2019-06-09 14:40:35 +0800

    Cors

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,6 +1,8 @@
 // 能发送 Ajax 请求的函数模块，函数的返回值是 Promise 对象
 import axios from 'axios'
 
+axios.defaults.withCredentials = true
+
 function ajax(url, data={}, method='GET') {
   if(method === 'GET') {
     let paramStr = ''
@@ -10,9 +12,9 @@ function ajax(url, data={}, method='GET') {
     if(paramStr) {
       paramStr = paramStr.substring(0, paramStr.length-1)
     }
-    return axios.get(url + '?' + paramStr)
+    return axios.get('http://localhost:4000' + url + '?' + paramStr)
   } else {
-    return axios.post(url, data)
+    return axios.post('http://localhost:4000' + url, data)
   }
 }
 
